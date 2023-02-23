997ccf2559fe09d85c2f39a8fac80f85482ec83e
Erez Hasson <erezhasson4@gmail.com>
2023-02-23 10:07:06 +0200

    shared-services

--- src/cmp-new/index.ts
+++ src/cmp-new/index.ts
@@ -4,15 +4,18 @@ export function cmpNew(_options: any): Rule {
   const name = _options.name;
 
   return (tree: Tree, _context: SchematicContext) => {
-    let appliedRules: Rule = externalSchematic('@schematics/angular', 'ng-new', {
-      name,
-      directory: name,
-    });
+    let appliedRules: Rule = null;
 
     if (!!_options.includeShared) {
       appliedRules = chain([schematic("shared-services", {}), appliedRules]);
     }
 
-    return appliedRules;
+    return chain([
+      externalSchematic("@schematics/angular", "ng-new", {
+        name,
+        directory: name,
+      }),
+      appliedRules
+    ]);
   };
 }
--- src/shared-services/index.ts
+++ src/shared-services/index.ts
@@ -1,14 +1,16 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import { chain, externalSchematic, Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 
 function sharedModuleRule(): Rule {
-
     return (tree, context) => {
-
+        return chain([
+            // externalSchematic("@angular/schematics", "ng-add",)
+        ])
     }
 }
 
+
 export function sharedServices(_options: any): Rule {
     return (tree: Tree, _context: SchematicContext) => {
-        console.log(_options)
+        const sharedRules: Rule = sharedModuleRule();
     };
 }
